824b0daa840cf2508534447ac007b2ff8026aa30
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-12-26 18:15:12 +0700

    feat: adjust page and meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
-<script lang="ts">
-	import { ThirdwebSvelteProvider } from '$/components/thirdweb-svelte-provider/index.js';
-	import '../app.css';
+<script>
 	import { ModeWatcher } from 'mode-watcher';
-	import { PUBLIC_THIRDWEB_CLIENT_ID } from '$env/static/public';
+	import '../app.css';
 </script>
 
 <ModeWatcher defaultMode="dark" />
-<ThirdwebSvelteProvider clientId={PUBLIC_THIRDWEB_CLIENT_ID}>
-	<slot />
-</ThirdwebSvelteProvider>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,1 @@
-<script lang="ts">
-	import ConnectWalletButton from '$/components/connect-wallet-button/connect-wallet-button.svelte';
-</script>
-
-<div class="twsv-flex twsv-h-screen twsv-w-screen twsv-items-center twsv-justify-center">
-	<ConnectWalletButton />
-</div>
+asdfasdfasdf
