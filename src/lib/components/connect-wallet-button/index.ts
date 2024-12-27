50cfe8c2e7e87067a7cdcbdd8e9f087bb706acec
Teodorus Nathaniel <53143942+teodorus-nathaniel@users.noreply.github.com>
2024-12-27 19:41:15 +0700

    feat: view home page (#25)
    * feat: adjust page and meta tags
    
    * feat: add andale font
    
    * feat: add inter font
    
    * feat: home page ui
    
    * feat: add href to try me button
    
    * feat: add sonner and copy to clipboard
    
    * chore: remove unnecessary id
    
    * fix: remove unused image
    
    * chore: set default rounding
    
    * chore: add essential meta tags
    
    * chore: remove duplicate classes
    
    * feat: add connect wallet to try me button
    
    * chore: update link
    
    * feat: add custom props for disconnect and export button

--- src/lib/components/connect-wallet-button/index.ts
+++ src/lib/components/connect-wallet-button/index.ts
@@ -1,6 +1,9 @@
 import type { ButtonProps } from '../ui/button/index.js';
 import ConnectWalletButton from './connect-wallet-button.svelte';
 
-type ConnectWalletButtonProps = ButtonProps;
+type ConnectWalletButtonProps = ButtonProps & {
+	disconnectButtonProps?: ButtonProps;
+	exportPrivateKeyButtonProps?: ButtonProps;
+};
 
 export { ConnectWalletButton, type ConnectWalletButtonProps };
