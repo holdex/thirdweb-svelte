aea16340e97d4935a329869245e826d8c7cbb07a
Teodorus Nathaniel <53143942+teodorus-nathaniel@users.noreply.github.com>
2025-01-06 20:13:10 +0700

    fix: use thirdweb-svelte in svelte v5 environment (#36)

--- src/lib/components/connect-wallet-modal/connect-wallet-modal.svelte
+++ src/lib/components/connect-wallet-modal/connect-wallet-modal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { mediaQuery } from 'svelte-legos';
+	import MediaQuery from 'svelte-media-queries';
 	import * as Dialog from '$/components/ui/dialog/index.js';
 	import * as Drawer from '$/components/ui/drawer/index.js';
 	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
@@ -18,8 +18,6 @@
 	export let walletConnect: $$Props['walletConnect'] = undefined;
 	export let chains: $$Props['chains'] = undefined;
 	export let wallets: NonNullable<$$Props['wallets']> = getDefaultWallets();
-
-	const isDesktop = mediaQuery('(min-width: 768px)');
 
 	let step: ConnectWalletModalStep = 'provider-selector';
 	let additionalProps: any = undefined;
@@ -55,80 +53,82 @@
 </script>
 
 <AutoConnect {chain} {chains} {wallets} />
-{#if $isDesktop}
-	<Dialog.Root {...$$restProps} bind:open>
-		<Dialog.Content {theme} class={cn('twsv-pb-4')}>
-			<Dialog.Header
-				class={cn(
-					'twsv-relative twsv-flex-row twsv-space-y-0',
-					showBackButton && 'twsv-justify-center'
-				)}
-			>
-				{#if showBackButton}
-					<Button
-						variant="ghost"
-						size="auto"
-						class="twsv-absolute -twsv-left-2 twsv-top-0 twsv-text-muted-foreground"
-						on:click={() => {
-							if (customBackClick) customBackClick();
-							else setStep('provider-selector');
-						}}
-					>
-						<ChevronLeft />
-					</Button>
-				{/if}
-				<Dialog.Title class="twsv-w-fit twsv-text-xl">{title}</Dialog.Title>
-			</Dialog.Header>
-			<ConnectWalletModalContent
-				{wallets}
-				{walletConnect}
-				{additionalProps}
-				{closeModal}
-				{chain}
-				{setStep}
-				{step}
-				{chains}
-				{setCustomBackClick}
-				setModalOpen={(newOpen) => (open = newOpen)}
-			/>
-		</Dialog.Content>
-	</Dialog.Root>
-{:else}
-	<Drawer.Root bind:open>
-		<Drawer.Content>
-			<Drawer.Header
-				class={cn(
-					'twsv-relative twsv-flex-row twsv-space-y-0',
-					showBackButton && 'twsv-justify-center'
-				)}
-			>
-				{#if showBackButton}
-					<Button
-						variant="ghost"
-						size="auto"
-						class="twsv-absolute -twsv-left-2 twsv-top-6 twsv-text-muted-foreground"
-						on:click={() => {
-							if (customBackClick) customBackClick();
-							else setStep('provider-selector');
-						}}
-					>
-						<ChevronLeft />
-					</Button>
-				{/if}
-				<Drawer.Title class="twsv-w-fit twsv-text-xl">{title}</Drawer.Title>
-			</Drawer.Header>
-			<ConnectWalletModalContent
-				{wallets}
-				{walletConnect}
-				{additionalProps}
-				{closeModal}
-				{chain}
-				{setStep}
-				{step}
-				{chains}
-				{setCustomBackClick}
-				setModalOpen={(newOpen) => (open = newOpen)}
-			/>
-		</Drawer.Content>
-	</Drawer.Root>
-{/if}
+<MediaQuery query="(min-width: 768px)" let:matches>
+	{#if matches}
+		<Dialog.Root {...$$restProps} bind:open>
+			<Dialog.Content {theme} class={cn('twsv-pb-4')}>
+				<Dialog.Header
+					class={cn(
+						'twsv-relative twsv-flex-row twsv-space-y-0',
+						showBackButton && 'twsv-justify-center'
+					)}
+				>
+					{#if showBackButton}
+						<Button
+							variant="ghost"
+							size="auto"
+							class="twsv-absolute -twsv-left-2 twsv-top-0 twsv-text-muted-foreground"
+							on:click={() => {
+								if (customBackClick) customBackClick();
+								else setStep('provider-selector');
+							}}
+						>
+							<ChevronLeft />
+						</Button>
+					{/if}
+					<Dialog.Title class="twsv-w-fit twsv-text-xl">{title}</Dialog.Title>
+				</Dialog.Header>
+				<ConnectWalletModalContent
+					{wallets}
+					{walletConnect}
+					{additionalProps}
+					{closeModal}
+					{chain}
+					{setStep}
+					{step}
+					{chains}
+					{setCustomBackClick}
+					setModalOpen={(newOpen) => (open = newOpen)}
+				/>
+			</Dialog.Content>
+		</Dialog.Root>
+	{:else}
+		<Drawer.Root bind:open>
+			<Drawer.Content>
+				<Drawer.Header
+					class={cn(
+						'twsv-relative twsv-flex-row twsv-space-y-0',
+						showBackButton && 'twsv-justify-center'
+					)}
+				>
+					{#if showBackButton}
+						<Button
+							variant="ghost"
+							size="auto"
+							class="twsv-absolute -twsv-left-2 twsv-top-6 twsv-text-muted-foreground"
+							on:click={() => {
+								if (customBackClick) customBackClick();
+								else setStep('provider-selector');
+							}}
+						>
+							<ChevronLeft />
+						</Button>
+					{/if}
+					<Drawer.Title class="twsv-w-fit twsv-text-xl">{title}</Drawer.Title>
+				</Drawer.Header>
+				<ConnectWalletModalContent
+					{wallets}
+					{walletConnect}
+					{additionalProps}
+					{closeModal}
+					{chain}
+					{setStep}
+					{step}
+					{chains}
+					{setCustomBackClick}
+					setModalOpen={(newOpen) => (open = newOpen)}
+				/>
+			</Drawer.Content>
+		</Drawer.Root>
+	{/if}
+</MediaQuery>
--- src/lib/components/export-private-key-modal/export-private-key-modal.svelte
+++ src/lib/components/export-private-key-modal/export-private-key-modal.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import * as Dialog from '$/components/ui/dialog/index.js';
 	import * as Drawer from '$/components/ui/drawer/index.js';
-	import { mediaQuery } from 'svelte-legos';
+	import MediaQuery from 'svelte-media-queries';
 	import type { ExportPrivateKeyModalProps } from './index.js';
 	import { cn } from '$/utils.js';
 	import ExportPrivateKeyModalContent from './export-private-key-modal-content.svelte';
 	import { getThirdwebSvelteContext } from '../thirdweb-svelte-provider/context.js';
 
-	const isDesktop = mediaQuery('(min-width: 768px)');
 	const { wallet } = getThirdwebSvelteContext();
 
 	type $$Props = ExportPrivateKeyModalProps;
@@ -15,22 +14,24 @@
 	export let theme: $$Props['theme'] = 'dark';
 </script>
 
-{#if $isDesktop}
-	<Dialog.Root {...$$restProps} bind:open>
-		<Dialog.Content {theme} class={cn('twsv-pb-4')}>
-			<Dialog.Header class={cn('twsv-relative twsv-flex-row twsv-space-y-0')}>
-				<Dialog.Title class="twsv-w-fit twsv-text-xl">Export Private Key</Dialog.Title>
-			</Dialog.Header>
-			<ExportPrivateKeyModalContent wallet={$wallet} {theme} />
-		</Dialog.Content>
-	</Dialog.Root>
-{:else}
-	<Drawer.Root bind:open>
-		<Drawer.Content>
-			<Drawer.Header class={cn('twsv-relative twsv-flex-row twsv-space-y-0')}>
-				<Drawer.Title class="twsv-w-fit twsv-text-xl">Export Private Key</Drawer.Title>
-			</Drawer.Header>
-			<ExportPrivateKeyModalContent wallet={$wallet} {theme} />
-		</Drawer.Content>
-	</Drawer.Root>
-{/if}
+<MediaQuery query="(min-width: 768px)" let:matches>
+	{#if matches}
+		<Dialog.Root {...$$restProps} bind:open>
+			<Dialog.Content {theme} class={cn('twsv-pb-4')}>
+				<Dialog.Header class={cn('twsv-relative twsv-flex-row twsv-space-y-0')}>
+					<Dialog.Title class="twsv-w-fit twsv-text-xl">Export Private Key</Dialog.Title>
+				</Dialog.Header>
+				<ExportPrivateKeyModalContent wallet={$wallet} {theme} />
+			</Dialog.Content>
+		</Dialog.Root>
+	{:else}
+		<Drawer.Root bind:open>
+			<Drawer.Content>
+				<Drawer.Header class={cn('twsv-relative twsv-flex-row twsv-space-y-0')}>
+					<Drawer.Title class="twsv-w-fit twsv-text-xl">Export Private Key</Drawer.Title>
+				</Drawer.Header>
+				<ExportPrivateKeyModalContent wallet={$wallet} {theme} />
+			</Drawer.Content>
+		</Drawer.Root>
+	{/if}
+</MediaQuery>
