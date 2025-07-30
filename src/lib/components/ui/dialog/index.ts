891e9b89bad82b1192b6d6bf1f58f52d1b9064a6
Teodorus Nathaniel <tnathanielk@gmail.com>
2025-07-31 01:10:31 +0700

    fix: export dialog props

--- src/lib/components/ui/dialog/index.ts
+++ src/lib/components/ui/dialog/index.ts
@@ -1,11 +1,13 @@
-import { Dialog as DialogPrimitive } from "bits-ui";
+import { Dialog as DialogPrimitive } from 'bits-ui';
 
-import Title from "./dialog-title.svelte";
-import Footer from "./dialog-footer.svelte";
-import Header from "./dialog-header.svelte";
-import Overlay from "./dialog-overlay.svelte";
-import Content from "./dialog-content.svelte";
-import Description from "./dialog-description.svelte";
+import Title from './dialog-title.svelte';
+import Footer from './dialog-footer.svelte';
+import Header from './dialog-header.svelte';
+import Overlay from './dialog-overlay.svelte';
+import Content from './dialog-content.svelte';
+import Description from './dialog-description.svelte';
+
+type DialogProps = DialogPrimitive.RootProps;
 
 const Root = DialogPrimitive.Root;
 const Trigger = DialogPrimitive.Trigger;
@@ -13,6 +15,7 @@ const Close = DialogPrimitive.Close;
 const Portal = DialogPrimitive.Portal;
 
 export {
+	type DialogProps,
 	Root,
 	Title,
 	Portal,
@@ -33,5 +36,5 @@ export {
 	Overlay as DialogOverlay,
 	Content as DialogContent,
 	Description as DialogDescription,
-	Close as DialogClose,
+	Close as DialogClose
 };
