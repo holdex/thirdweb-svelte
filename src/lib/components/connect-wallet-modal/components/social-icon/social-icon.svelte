93d8ba7f1911c11743cff5565f07c789002cefb5
Teodorus Nathaniel <tnathanielk@gmail.com>
2025-07-31 00:50:09 +0700

    feat: update $$props

--- src/lib/components/connect-wallet-modal/components/social-icon/social-icon.svelte
+++ src/lib/components/connect-wallet-modal/components/social-icon/social-icon.svelte
@@ -2,16 +2,12 @@
 	import type { HTMLImgAttributes } from 'svelte/elements';
 	import { type SocialIconName, socialIcons } from './index.js';
 
-	type $$Props = Omit<HTMLImgAttributes, 'src' | 'alt'> & {
+	let {
+		iconName,
+		...rest
+	}: Omit<HTMLImgAttributes, 'src' | 'alt'> & {
 		iconName: SocialIconName;
-	};
-
-	interface Props {
-		iconName: SocialIconName;
-		[key: string]: any;
-	}
-
-	let { iconName, ...rest }: Props = $props();
+	} = $props();
 </script>
 
 <img {...rest} src={socialIcons[iconName]} alt="" />
