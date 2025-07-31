44097f83e846304a8d3f0e98e20ff0b070c089f5
Teodorus Nathaniel <tnathanielk@gmail.com>
2025-07-31 12:40:48 +0700

    fix: remove outline in dialog container

--- src/lib/components/ui/dialog/dialog-content.svelte
+++ src/lib/components/ui/dialog/dialog-content.svelte
@@ -25,7 +25,7 @@
 		bind:ref
 		{...restProps}
 		class={cn(
-			'data-[state=open]:twsv-animate-in data-[state=closed]:twsv-animate-out data-[state=closed]:twsv-fade-out-0 data-[state=open]:twsv-fade-in-0 data-[state=closed]:twsv-zoom-out-95 data-[state=open]:twsv-zoom-in-95 data-[state=closed]:twsv-slide-out-to-left-1/2 data-[state=closed]:twsv-slide-out-to-top-[48%] data-[state=open]:twsv-slide-in-from-left-1/2 data-[state=open]:twsv-slide-in-from-top-[48%] twsv-fixed twsv-left-[50%] twsv-top-[50%] twsv-z-50 twsv-grid twsv-w-full twsv-max-w-sm twsv-translate-x-[-50%] twsv-translate-y-[-50%] twsv-gap-4 twsv-rounded-lg twsv-border twsv-border-border twsv-bg-background twsv-shadow-lg twsv-duration-200 sm:twsv-rounded-2xl md:twsv-w-full',
+			'data-[state=open]:twsv-animate-in data-[state=closed]:twsv-animate-out data-[state=closed]:twsv-fade-out-0 data-[state=open]:twsv-fade-in-0 data-[state=closed]:twsv-zoom-out-95 data-[state=open]:twsv-zoom-in-95 data-[state=closed]:twsv-slide-out-to-left-1/2 data-[state=closed]:twsv-slide-out-to-top-[48%] data-[state=open]:twsv-slide-in-from-left-1/2 data-[state=open]:twsv-slide-in-from-top-[48%] twsv-fixed twsv-left-[50%] twsv-top-[50%] twsv-z-50 twsv-grid twsv-w-full twsv-max-w-sm twsv-translate-x-[-50%] twsv-translate-y-[-50%] twsv-gap-4 twsv-rounded-lg twsv-border twsv-border-border twsv-bg-background twsv-shadow-lg twsv-outline-none twsv-duration-200 sm:twsv-rounded-2xl md:twsv-w-full',
 			theme === 'dark' && 'dark',
 			className
 		)}
